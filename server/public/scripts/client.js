a80cc82dc5be0533e769841b1137567aa0f1553b
Karl Beck <karljohnbeck@gmail.com>
2020-08-07 20:40:47 -0500

    compleated the GET route to the database

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,14 +4,29 @@ $(document).ready(readyNow)
 
 function readyNow() {
     console.log('JQ')
+    $('#submitNoteBtn').on('click', submitNewTask)
+    getTasks()
 }
 
 
 // GET
+function getTasks() {
+    console.log('inside getTasks()')
+    $.ajax({
+        method: 'GET',
+        url: '/toDos'
+    }).then(function (response){
+        console.log(response)
+    }). catch(function (error) {
+        console.log('error in the server', error)
+    });
+}
 
 
 //POST
-
+function submitNewTask() {
+    console.log('weeeeeeee')
+} 
 
 // DELETE
 
--- server/router/todo.router.js
+++ server/router/todo.router.js
@@ -6,7 +6,15 @@ const toDoRouter = express.Router();
 // GET DB connection with pg
 const pool = require('../modules/pool')
 // GET
-
+toDoRouter.get('/', (req,res) => {
+    let queryText = 'SELECT * FROM "todolist";'
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log('error in GET: ', error)
+        res.sendStatus(500);
+    });
+});
 
 //POST
 

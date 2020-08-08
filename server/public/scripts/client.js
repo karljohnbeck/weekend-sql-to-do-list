8fc50cf2b93241b3cb4d9a7dea86cfca511e5ca0
Karl Beck <karljohnbeck@gmail.com>
2020-08-07 21:37:09 -0500

    compleated post functionality

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -26,6 +26,20 @@ function getTasks() {
 //POST
 function submitNewTask() {
     console.log('weeeeeeee')
+    let newRow = {
+        task: $('#taskIn').val(),
+        note: $('#noteIn').val(),
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/toDos',
+        data: newRow
+    }).then(function (response){
+        console.log(response)
+        getTasks()
+    }).catch(function (error) {
+        console.log('error in POST: ', error)
+    })
 } 
 
 // DELETE
--- server/router/todo.router.js
+++ server/router/todo.router.js
@@ -17,7 +17,20 @@ toDoRouter.get('/', (req,res) => {
 });
 
 //POST
-
+toDoRouter.post('/', (req,res) => {
+    let newToDo = req.body;
+    let queryText = `
+    INSERT INTO todolist ("task", "note")
+    VALUES ($1, $2);
+    `;
+    pool.query(queryText, [newToDo.task, newToDo.note])
+.then((result) => {
+    res.sendStatus(201)
+}).catch((error) => {
+    console.log('Error in POST: ', error)
+    res.sendStatus(500);
+})
+})
 
 // DELETE
 

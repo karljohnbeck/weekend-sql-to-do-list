eb6912b2a2ef6d9b3ee1fd224935a98270fc9ec0
Karl Beck <karljohnbeck@gmail.com>
2020-08-08 10:03:01 -0500

    added database.sql

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE toDoList (
+    "id" serial PRIMARY KEY,
+ "task" varchar(255) NOT NULL,
+    "note" varchar(225),
+    "completed" BOOLEAN DEFAULT false
+);
+
+INSERT INTO todolist ("task", "note")
+VALUES ('Make Bed', 'do this before you eat cookies'),
+('eat', 'eat those leftovers in the fridge');

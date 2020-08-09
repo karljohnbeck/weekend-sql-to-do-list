2b4d14dfdc9b7bee02721c520ed1085b1ee8fbe3
Karl Beck <karljohnbeck@gmail.com>
2020-08-09 13:01:08 -0500

    added notes in client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,7 @@ $(document).ready(readyNow)
 
 function readyNow() {
     console.log('JQ')
+    // click listeners for submit, complete and 
     $('#submitNoteBtn').on('click', submitNewTask)
     $('.completeDelete').on('click', '.completeBtn', markAsCompleted)
     $('.completeDelete').on('click', '.deleteBtn', deleteTask)
@@ -11,7 +12,7 @@ function readyNow() {
 }
 
 
-// GET
+// GET the data from the database, stop by the server first
 function getTasks() {
     console.log('inside getTasks()')
     $.ajax({
@@ -19,6 +20,7 @@ function getTasks() {
         url: '/toDos'
     }).then(function (response) {
         console.log(response)
+        // go the append function
         AppendToDo(response)
     }).catch(function (error) {
         console.log('error in the server', error)
@@ -29,19 +31,23 @@ function getTasks() {
 //POST
 function submitNewTask() {
     console.log('weeeeeeee')
+    // make sure there is an input value in at least Task input
     if ($('#taskIn').val() === '') {
         alert('Please fill out the Task field before submitting')
     } else {
+        // store the values in an object
         let newRow = {
             task: $('#taskIn').val(),
             note: $('#noteIn').val(),
         }
+        // send it to the server 
         $.ajax({
             method: 'POST',
             url: '/toDos',
             data: newRow
         }).then(function (response) {
             console.log(response)
+            // re-append for updated database
             getTasks()
         }).catch(function (error) {
             console.log('error in POST: ', error)
@@ -52,8 +58,10 @@ function submitNewTask() {
 // DELETE
 function deleteTask() {
     console.log('wahooooooo')
+    // located the id data we put into THIS tr
     console.log(($(this).closest('tr').data().id))
     let rowId = $(this).closest('tr').data().id
+    // url is the saved value
     $.ajax({
         method: 'DELETE',
         url: `/toDos/${rowId}`
@@ -71,6 +79,7 @@ function markAsCompleted() {
     console.log(($(this).closest('tr').data().id))
     let rowId = $(this).closest('tr').data().id
     let taskStatus = true;
+    // send over the data id in the url and the taskStatus in the data
     $.ajax({
         method: 'PUT',
         url: `/toDos/${rowId}`,
@@ -83,12 +92,16 @@ function markAsCompleted() {
     })
 }
 
-// append to the dom
+// append to the dom function 
 
 function AppendToDo(value) {
+    // clear the dom 
     $('.completeDelete').empty()
+    // loop over the data ajax brought from the database 
     for (let i = 0; i < value.length; i++) {
         const toDo = value[i];
+        // this if block either puts the data in the to do table 
+        // or done table based on completed key status
         if (toDo.completed === false) {
             console.log(toDo)
             $('#toDoTableRows').append(`
